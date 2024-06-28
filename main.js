5156a89062c01a08de18e52a64b1fd1ce0b7c618
Nifemi Adewusi <nifemiadewusi@gmail.com>
2024-06-29 00:31:43 +0100

    Increase image width

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ export const aiDiv = (data) => {
   return `
   <!-- AI Chat -->
           <div class="flex gap-2 justify-end">
-            <pre class="bg-gemRegular/40 text-gemDeep p-1 rounded-md shadow-md whitespace-pre-wrap">
+            <pre class="bg-gemRegular/40 text-gemDeep p-1 rounded-lg shadow-md whitespace-pre-wrap">
               ${data}
             </pre>
             <img
